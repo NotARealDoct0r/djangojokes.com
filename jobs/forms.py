afbb5a71b512b36b8138e595fc89d63c945ba4da
InJun <123285589+NotARealDoct0r@users.noreply.github.com>
2025-02-06 22:27:10 -0500

    improved the job application form by adding widgets + attributes

--- jobs/forms.py
+++ jobs/forms.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django import forms
 
 class JobApplicationForm(forms.Form):
@@ -8,7 +9,7 @@ class JobApplicationForm(forms.Form):
         ('contract_work', 'Contract work')
     )
 
-    days = (
+    DAYS = (
         ('monday', 'MON'),
         ('tuesday', 'TUES'),
         ('wednesday', 'WED'),
@@ -18,20 +19,42 @@ class JobApplicationForm(forms.Form):
         ('sunday', 'SUN')
     )
 
-    first_name = forms.CharField()
+    YEARS = range(datetime.now().year, datetime.now().year+2)
+
+    first_name = forms.CharField(
+        widget=forms.TextInput(attrs={'autofocus': True})
+    )
     last_name = forms.CharField()
     email = forms.EmailField()
-    website = forms.URLField(required=False)
+    website = forms.URLField(
+        required=False,
+        widget=forms.URLInput(
+            attrs={'placeholder': 'https://www.example.com', 'size': '50'}
+        )
+    )
     employment_type = forms.ChoiceField(choices= JOB_TYPES)
     start_date = forms.DateField(
-        help_text='The earliest date you can start working.'
+        help_text='The earliest date you can start working.',
+        widget=forms.SelectDateWidget(
+            years=YEARS
+        )
     )
-    available_days = forms.MultipleChoiceField(
-        choices= days, 
-        help_text='Select all days that you can work.'
+    available_days = forms.TypedMultipleChoiceField(
+        choices=DAYS,
+        coerce=int,
+        help_text='Check all days that you can work.',
+        widget=forms.CheckboxSelectMultiple(
+            attrs={'checked':True}
+        )
     )
-    desired_hourly_wage = forms.DecimalField()
-    cover_letter = forms.CharField()
+    desired_hourly_wage = forms.DecimalField(
+        widget=forms.NumberInput(
+            attrs={'min':'10.00', 'max':'100.00', 'step':'.25'}
+        )
+    )
+    cover_letter = forms.CharField(
+        widget=forms.Textarea(attrs={'cols': '75', 'rows': '5'})
+    )
     confirmation = forms.BooleanField(
         label='I certify that the information I have provided is true.'
     )
