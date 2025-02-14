96f431abdd5f9c0a9f6700ad2619883c6a53df99
InJun <123285589+NotARealDoct0r@users.noreply.github.com>
2025-02-14 16:38:05 -0500

    Associated Users with Jokes

--- jokes/views.py
+++ jokes/views.py
@@ -13,6 +13,12 @@ class JokeCreateView(CreateView):
     # fields = ['question', 'answer']
     form_class = JokeForm
 
+    # 'instance' holds the data in the 'form' object
+    # 'return' handles saving the form + redirecting to the success URL
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
 class JokeDeleteView(DeleteView):
     model = Joke
     success_url = reverse_lazy('jokes:list')
