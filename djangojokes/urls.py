bf02583ed3f668825c608ac079cd787019ae6fb6
InJun <123285589+NotARealDoct0r@users.noreply.github.com>
2025-02-13 19:43:54 -0500

    Got started with django-allauth

--- djangojokes/urls.py
+++ djangojokes/urls.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-     path('admin/doc/', include('django.contrib.admindocs.urls')),
+    # Admin
+    path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
-     path('jobs/', include('jobs.urls')),
+    
+    # User Management
+    path('account/', include('allauth.urls')),
+    
+    # Local Apps
+    path('jobs/', include('jobs.urls')),
     path('jokes/', include('jokes.urls')),
     path('', include('pages.urls')),
 ]
