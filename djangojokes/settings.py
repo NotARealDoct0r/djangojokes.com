8c9599d9911c7a101b9fc8638f4276ddbf2b6bfe
InJun <123285589+NotARealDoct0r@users.noreply.github.com>
2025-02-27 18:42:46 -0500

    reverted back to local Databases settings (until Heroku subscription issue is resolved)

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -110,18 +110,18 @@ WSGI_APPLICATION = 'djangojokes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'jokes',
-#         'USER': 'postgres',
-#         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-#         'HOST': 'localhost',
-#         'PORT': 5432
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jokes',
+        'USER': 'postgres',
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': 5432
+    }
+}
 
-DATABASES = { 'default' : dj_database_url.config()}
+# DATABASES = { 'default' : dj_database_url.config()}
 
 # EMAIL
 SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
