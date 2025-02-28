539db6c0298a491d8edac4a65a53ed0ed553578a
InJun <123285589+NotARealDoct0r@users.noreply.github.com>
2025-02-27 19:33:28 -0500

    Customized Titles + Headers, created a custom ModelAdmin base class, & customized Django Admin list view

--- jokes/admin.py
+++ jokes/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
+from common.admin import DjangoJokesAdmin
 
 from .models import Category, Joke, JokeVote, Tag
 
 @admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(DjangoJokesAdmin):
     model = Category
     list_display = ['category', 'created', 'updated']
 
@@ -13,18 +14,23 @@ class CategoryAdmin(admin.ModelAdmin):
         return ()
 
 @admin.register(Joke)
-class JokeAdmin(admin.ModelAdmin):
+class JokeAdmin(DjangoJokesAdmin):
     model = Joke
-    list_display = ['question', 'created', 'updated']
+
+    # List Attributes
+    date_hierarchy = 'updated'
+    list_display = ['question', 'category', 'updated']
+    list_filter = ['updated', 'category', 'tags']
+    ordering = ['-updated']
+    search_fields = ['question', 'answer']
 
     def get_readonly_fields(self, request, obj=None):
         if obj: # editing an existing object
             return ('slug', 'created', 'updated')
-
         return ()
 
 @admin.register(JokeVote)
-class JokeVoteAdmin(admin.ModelAdmin):
+class JokeVoteAdmin(DjangoJokesAdmin):
     model = JokeVote
     list_display = ['joke', 'user', 'vote']
 
@@ -34,7 +40,7 @@ class JokeVoteAdmin(admin.ModelAdmin):
         return ()
 
 @admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
+class TagAdmin(DjangoJokesAdmin):
     model = Tag
     list_display = ['tag', 'created', 'updated']
 
