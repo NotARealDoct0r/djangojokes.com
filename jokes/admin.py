f294d6d2ad02a2b36a69edd60a71aab35ab5599b
InJun <123285589+NotARealDoct0r@users.noreply.github.com>
2025-03-01 16:35:17 -0500

    Customized Django Admin form views, modified CustomUserAdmin class (list attributes, user form, PW field, new user form, save buttons placement), added link to change PW form,  unregistered 'social' models, & created a group with permissions and added user to the group

--- jokes/admin.py
+++ jokes/admin.py
@@ -24,10 +24,17 @@ class JokeAdmin(DjangoJokesAdmin):
     ordering = ['-updated']
     search_fields = ['question', 'answer']
 
+    # Form Attributes
+    autocomplete_fields = ['tags', 'user']
+    radio_fields = { 'category': admin.HORIZONTAL}
+
     def get_readonly_fields(self, request, obj=None):
         if obj: # editing an existing object
-            return ('slug', 'created', 'updated')
+            return ('slug', 'created', 'updated', 'vote_summary')
         return ()
+    
+    def vote_summary(self, obj):
+        return f'{obj.num_votes} votes. Rating: {obj.rating}.'
 
 @admin.register(JokeVote)
 class JokeVoteAdmin(DjangoJokesAdmin):
@@ -43,6 +50,7 @@ class JokeVoteAdmin(DjangoJokesAdmin):
 class TagAdmin(DjangoJokesAdmin):
     model = Tag
     list_display = ['tag', 'created', 'updated']
+    search_fields = ['tag']
 
     def get_readonly_fields(self, request, obj=None):
         if obj: # editing an existing object
